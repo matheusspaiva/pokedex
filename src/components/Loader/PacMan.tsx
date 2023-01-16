6028432cf78ba174352daf26e285519de8e3d3d0
Matheus Silva Paiva <79916657+matheusspaiva@users.noreply.github.com>
2023-01-15 21:15:34 -0300

    Teste de paleta de cores

--- src/components/Loader/PacMan.tsx
+++ src/components/Loader/PacMan.tsx
@@ -1,15 +1,8 @@
-import React, { CSSProperties }  from 'react'
+import React  from 'react'
 import PacmanLoader from 'react-spinners/PacmanLoader';
 
 const PacMan: React.FC = () => {
 
-    const override: CSSProperties = {
-        display: "block",
-        margin: "0 auto",
-        borderColor: "red",
-        marginTop : '10px'
-      };
-      
 
     return (
         <div style={{display: "flex", flexWrap:"wrap", alignContent:"center"}}>    
