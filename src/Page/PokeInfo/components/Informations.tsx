21831b317e7b8d4932daf65aaaacb7a02468ebeb
Matheus Silva Paiva <79916657+matheusspaiva@users.noreply.github.com>
2023-01-23 21:46:31 -0300

    adicão de sistema de status

--- src/Page/PokeInfo/components/Informations.tsx
+++ src/Page/PokeInfo/components/Informations.tsx
@@ -1,44 +1,64 @@
+import { useState } from "react";
+import { ProgressBar } from "react-bootstrap";
+import { FaChartBar } from "react-icons/fa";
 import { Modal } from "../../../components/Modal/Modal";
 import { useModal } from "../../../components/Modal/useModal";
 import PokeInfo from "../types"
 import './../index.css';
 
-const Informations: React.FC<{info:PokeInfo}> = ({info}) => {
+const Informations: React.FC<{ info: PokeInfo }> = ({ info }) => {
     const { isShown, toggle, setIsShown } = useModal();
+    const [infoShow, setInfoShow] = useState<boolean>(false)
+    const [statusShow, setStatusShow] = useState<boolean>(false)
+
     return (
 
         <>
-        <div style={{marginTop:"10px"}}>
-            {info && 
-            <>
-            <div className="info-box"> 
-                <div>
-                    <label>Peso: {info.weight/10} Kg</label>
-                </div>
-                <div>
-                    <label>Altura: {info.height/10} m</label>
-                </div>
-            </div>
-            <div className="move-box" style={{backgroundColor:"gray", marginTop:"10px"}}>
-                <label>Movimentos : </label>
-                {info.moves.slice(0,4).map(item=>
-                    <div key={item.move.name}>
-                    {item.move.name}
-                    </div>
-                    )}
-                <p className="extra-moves" onClick={()=> setIsShown(true)}>. . .</p>  
-            </div>
+            <div style={{ marginTop: "10px" }}>
+                {info &&
+                    <>
+                        <div className="info-box">
+                            <div>
+                                <div>
+                                    <label>Peso: {info.weight / 10} Kg</label>
+                                </div>
+                                <div>
+                                    <label>Altura: {info.height / 10} m</label>
+                                </div>
+                            </div>
+                            <div className="info-stats" onClick={() => {setIsShown(true); setInfoShow(false); setStatusShow(true)}}>
+                                <h1><FaChartBar style={{color: 'red'}} /></h1>
+                            </div>
 
-            </>
-            }
-        </div>
-        <Modal isShown={isShown} hide={toggle} modalContent={<>
-            {info.moves.map(item=>
-                    < div key={item.move.name}>
-                    {item.move.name}
+                        </div>
+                        <div className="move-box" style={{ backgroundColor: "gray", marginTop: "10px" }}>
+                            <label>Movimentos : </label>
+                            {info.moves.slice(0, 4).map(item =>
+                                <div key={item.move.name}>
+                                    {item.move.name}
+                                </div>
+                            )}
+                            <p className="extra-moves" onClick={() => {setIsShown(true); setInfoShow(true); setStatusShow(false)}}>. . .</p>
+                        </div>
+
+                    </>
+                }
+            </div>
+            <Modal isShown={isShown &&statusShow} hide={toggle} modalContent={<>
+                {info.stats.map(item =>
+                    < div key={item.stat.name}>
+                      {item.stat.name} <ProgressBar variant="danger" max={255} now={item.base_stat} label={`${item.base_stat}`}/>  
                     </div>
-                    )}
-        </>} headerText={"Lista de Habilidades Completa"} />
+                )}
+            </>} headerText={"Status basico"} />
+
+            <Modal isShown={isShown && infoShow} hide={toggle} modalContent={<>
+                {info.moves.map(item =>
+                    < div key={item.move.name}>
+                        {item.move.name}
+                    </div>
+                )}
+            </>} headerText={"Lista de Habilidades Completa"} />
         </>
     )
 }
